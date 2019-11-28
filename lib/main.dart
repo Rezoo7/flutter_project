6d2ffb84ddbd74440c816df07e412c5b2d630034
Maxime <maxime.guigourez@gmail.com>
2019-11-28 14:47:35 +0100

    Interface premiere activitée

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -26,13 +25,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,24 +32,75 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body:
+      Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                "Lieux : ",
+              ),
+              SizedBox(width: 35.0),
+              new Flexible(
+                child: new TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Entrez une Localisation ',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0,),
+          Row(
+            children: <Widget>[
+              Text(
+                "Recherche : ",
+              ),
+              SizedBox(width: 5.0),
+              new Flexible(
+                child: new TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Bars, Restaurants... ',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 28.0,),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Resultats'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),
+                  );
+                },
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
